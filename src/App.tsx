0e3aeafdfbbef868a7994919d79ad87988aa191a
rbpolim <rbpolim@gmail.com>
2024-02-16 22:18:39 -0300

    feat: install shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-export function App() {
+import './global.css'
+
+import { Button } from '@/components/ui/button'
+
+export const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button>Enviar</Button>
     </div>
   )
 }
