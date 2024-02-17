d5b64436305f8ee78a332409381e7dec2c00e31c
rbpolim <rbpolim@gmail.com>
2024-02-16 22:28:40 -0300

    feat: install eslint and some plugins

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 
 export const App = () => {
   return (
-    <div>
+    <div className="flex h-screen items-center justify-center">
       <h1>Hello World</h1>
       <Button>Enviar</Button>
     </div>
