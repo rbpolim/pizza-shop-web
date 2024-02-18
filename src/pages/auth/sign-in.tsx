0b12e781e290e0aa7fb29de38aba9ac0935e95fb
rbpolim <rbpolim@gmail.com>
2024-02-17 21:31:07 -0300

    feat: onSubmit using react-hook-form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,25 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const schema = z.object({
+  email: z.string().email(),
+})
+
+type FormData = z.infer<typeof schema>
+
 export const SignIn = () => {
+  const form = useForm<FormData>()
+  const { isLoading } = form.formState
+
+  const onSubmit = (values: FormData) => {
+    console.log(values)
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,12 +33,12 @@ export const SignIn = () => {
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
-          <form className="space-y-4">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...form.register('email')} />
             </div>
-            <Button className="w-full" type="submit">
+            <Button type="submit" disabled={isLoading} className="w-full">
               Acessar painel
             </Button>
           </form>
