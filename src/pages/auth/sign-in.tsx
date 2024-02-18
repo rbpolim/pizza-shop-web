10666c78cbd67a8e12d182c14df787d8204307f2
rbpolim <rbpolim@gmail.com>
2024-02-18 19:39:43 -0300

    feat: install toast sonner

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -17,13 +19,22 @@ export const SignIn = () => {
   const { isLoading } = form.formState
 
   const onSubmit = (values: FormData) => {
-    console.log(values)
+    try {
+      console.log(values)
+      toast.success('Login realizado com sucesso!')
+    } catch (error) {
+      console.error(error)
+      toast.error('Erro ao realizar login!')
+    }
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
